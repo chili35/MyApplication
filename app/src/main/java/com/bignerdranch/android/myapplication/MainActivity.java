98c5fc4861421c60014a4364c0cb7f27b93c39b3
chili35 <1251932725@qq.com>
2017-04-03 20:16:50 +0800

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.bignerdranch.android.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mEnterButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEnterButton = (Button)findViewById(R.id.enter_button);
+        mEnterButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(MainActivity.this,PositionActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
